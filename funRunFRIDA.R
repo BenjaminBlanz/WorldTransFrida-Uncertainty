b3c121242bf3a91e759f3b1e20e7732dc7e06c73
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2024-12-20 10:53:38 +0100

    use location.frida and name.fridaInputFile from global env

--- funRunFRIDA.R
+++ funRunFRIDA.R
@@ -3,7 +3,8 @@
 #
 
 # fun write frida input ####
-writeFRIDAInput <- function(variables,values,location.frida,name.fridaInputFile){
+# uses location.frida and name.fridaInputFile from the global env.
+writeFRIDAInput <- function(variables,values){
 	parmValues <- data.frame(Variable=variables,Value=values)
 	write.table(parmValues,file = file.path(location.frida,'Data',name.fridaInputFile),
 							row.names = F,col.names = F,sep=',')
@@ -13,7 +14,7 @@ writeFRIDAInput <- function(variables,values,location.frida,name.fridaInputFile)
 # Uses sampleParms,samplePoints,location.frida, and name.fridaInputFile
 # from the global environment!
 runFridaParmsByIndex <- function(i){
-	writeFRIDAInput(sampleParms$Variable,samplePoints[,i],location.frida,name.fridaInputFile)
+	writeFRIDAInput(sampleParms$Variable,samplePoints[,i])
 	system(paste(file.path(location.stella,'stella_simulator'),'-i','-x','-q',
 							 file.path(location.frida,'FRIDA.stmx')),
 				 ignore.stdout = T,ignore.stderr = T,wait = T)
