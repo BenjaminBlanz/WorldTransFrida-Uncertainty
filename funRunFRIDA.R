9e98e974295832fbf668e20857f1a1e32ec2a33b
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2024-12-20 11:00:22 +0100

    function to run frida with default parms

--- funRunFRIDA.R
+++ funRunFRIDA.R
@@ -10,11 +10,23 @@ writeFRIDAInput <- function(variables,values){
 							row.names = F,col.names = F,sep=',')
 }
 
-# fun runFridaOnceByIndex ####
+# fun runFridaParmsByIndex ####
 # Uses sampleParms,samplePoints,location.frida, and name.fridaInputFile
-# from the global environment!
+# from the global environment
 runFridaParmsByIndex <- function(i){
 	writeFRIDAInput(sampleParms$Variable,samplePoints[,i])
+	system(paste(file.path(location.stella,'stella_simulator'),'-i','-x','-q',
+							 file.path(location.frida,'FRIDA.stmx')),
+				 ignore.stdout = T,ignore.stderr = T,wait = T)
+	return(read.csv(file.path(location.frida,'Data',name.fridaOutputFile)))
+}
+
+# fun runFridaDefaultParms ####
+# Uses sampleParms,samplePoints,location.frida, and name.fridaInputFile
+# from the global environment
+runFridaDefaultParms <- function(){
+	system(paste('rm',file.path(location.frida,'data',name.fridaInputFile)),
+				 ignore.stdout = T, ignore.stderr = T)
 	system(paste(file.path(location.stella,'stella_simulator'),'-i','-x','-q',
 							 file.path(location.frida,'FRIDA.stmx')),
 				 ignore.stdout = T,ignore.stderr = T,wait = T)
