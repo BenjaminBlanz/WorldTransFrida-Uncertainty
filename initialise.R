00fb7ccc6f4989bbcf3a5f731da2aedc62e4f95a
Benjamin Blanzn <benjamin.blanz@uni-hamburg.de>
2024-12-26 08:28:22 +0100

    initialise all needed functions

---
+++ initialise.R
@@ -0,0 +1,19 @@
+suppressPackageStartupMessages({
+	library(caret,quietly=T,warn.conflicts = F) # to find linear combinations and remove them in the calib dat
+	library(matrixcalc,quietly=T,warn.conflicts = F) # to test positive definitnes of cov matrix
+	library(imputeTS,quietly=T,warn.conflicts = F) # used for interpolating missing values
+	library(Rmpfr,quietly=T,warn.conflicts = F) # use to calculate the likelihood from loglikelihood
+	library(parallel,quietly=T,warn.conflicts = F)
+	library(optimx,quietly=T,warn.conflicts = F)
+	library(Rmpfr,quietly=T,warn.conflicts = F)
+	library(MASS,quietly=T,warn.conflicts = F)
+	library(spatstat.explore,quietly=T,warn.conflicts = F) # used for the quantile.density function
+})
+
+source('config.R')
+source('funRunFRIDA.R')
+source('funPlot.R')
+source('config.R')
+source('funParmSpace.R')
+
+
