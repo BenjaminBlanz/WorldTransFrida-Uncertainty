5659f28d0d83f85a1b6f25b7517c06bce5d5a167
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2024-12-28 09:14:41 +0100

    do not stop and start the cluster unnessesarily. this is expensive with many threads

--- clusterHelp.R
+++ clusterHelp.R
@@ -5,52 +5,54 @@ if(!exists('location.output')){
 	stop('run this only after config.R\n')
 }
 
-# cluster cleanup ####
+# do not stop and start the cluster, instead just reinitialise
 if(exists('cl')){
-	try(stopCluster(cl),silent = T)
+	if(exists('sampleParms')){clusterExport(cl,list('sampleParms'))}
+	if(exists('calDat')){clusterExport(cl,list('calDat'))}
+	if(exists('resSigma')){clusterExport(cl,list('resSigma'))}
+	gobble <- clusterEvalQ(cl,tools::psnice(value=15))
+	gobble <- clusterExport(cl,list('location.output','baseWD',
+																	'chunkSizePerWorker','workDirBasename'))
+	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'initialise.R')))
+} else {
+	# cluster setup ####
+	cat('cluster setup...')
+	
+	if(!file.exists('workerDirs')){
+		source('setupTMPFS.R')
+	}
+	
+	baseWD <- getwd()
+	workDirBasename <- 'workDir_'
+	# start cluster
+	if(clusterType=='fork'){
+		cl <- makeForkCluster(numWorkers)
+	} else if (clusterType=='psock'){
+		cl <- makePSOCKcluster(numWorkers)
+	}
+	gobble <- clusterEvalQ(cl,tools::psnice(value=15))
+	gobble <- clusterExport(cl,list('location.output','baseWD',
+												'chunkSizePerWorker','workDirBasename'))
+	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'initialise.R')))
+	# make working directories
+	workers <- 1:length(cl)
+	gobble <- clusterApply(cl,workers,function(i){
+		workerID <- i
+		system(paste('rm -r',file.path('workerDirs',paste0(workDirBasename,i))),ignore.stdout = T,ignore.stderr = T)
+		dir.create(file.path('workerDirs',paste0(workDirBasename,i)),showWarnings = F,recursive = T)
+		setwd(file.path('workerDirs',paste0(workDirBasename,i)))
+	})
+	# clusterEvalQ(cl,getwd())
+	# copy over the model and simulator
+	gobble <- clusterApply(cl,workers,function(i){
+		file.copy(file.path(baseWD,location.frida),getwd(),recursive=T)
+		file.copy(file.path(baseWD,location.stella),getwd(),recursive=T)
+		file.copy(file.path(baseWD,'frida_info.csv'),getwd())
+	})
+	
+	# copy extra vars if present (for restarting the cluster during testing)
+	if(exists('sampleParms')){clusterExport(cl,list('sampleParms'))}
+	if(exists('calDat')){clusterExport(cl,list('calDat'))}
+	if(exists('resSigma')){clusterExport(cl,list('resSigma'))}
+	cat('done\n')
 }
-
-# cluster setup ####
-cat('cluster setup...')
-
-if(!file.exists('workerDirs')){
-	source('setupTMPFS.R')
-}
-
-baseWD <- getwd()
-workDirBasename <- 'workDir_'
-# start cluster
-if(clusterType=='fork'){
-	cl <- makeForkCluster(numWorkers)
-} else if (clusterType=='psock'){
-	cl <- makePSOCKcluster(numWorkers)
-}
-gobble <- clusterEvalQ(cl,tools::psnice(value=15))
-gobble <- clusterExport(cl,list('location.output','baseWD',
-											'chunkSizePerWorker','workDirBasename'))
-gobble <- clusterEvalQ(cl,source(file.path(baseWD,'initialise.R')))
-# make working directories
-workers <- 1:length(cl)
-gobble <- clusterApply(cl,workers,function(i){
-	workerID <- i
-	system(paste('rm -r',file.path('workerDirs',paste0(workDirBasename,i))),ignore.stdout = T,ignore.stderr = T)
-	dir.create(file.path('workerDirs',paste0(workDirBasename,i)),showWarnings = F,recursive = T)
-	setwd(file.path('workerDirs',paste0(workDirBasename,i)))
-})
-# clusterEvalQ(cl,getwd())
-# copy over the model and simulator
-gobble <- clusterApply(cl,workers,function(i){
-	file.copy(file.path(baseWD,location.frida),getwd(),recursive=T)
-	file.copy(file.path(baseWD,location.stella),getwd(),recursive=T)
-	file.copy(file.path(baseWD,'frida_info.csv'),getwd())
-})
-
-# copy extra vars if present (for restarting the cluster during testing)
-if(exists('sampleParms')){clusterExport(cl,list('sampleParms'))}
-if(exists('calDat')){clusterExport(cl,list('calDat'))}
-if(exists('resSigma')){clusterExport(cl,list('resSigma'))}
-
-
-
-
-cat('done\n')
