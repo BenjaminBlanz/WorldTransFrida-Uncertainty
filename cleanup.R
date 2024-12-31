656f6e28327be39b45de514e83fecc2d32a8353a
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2024-12-31 14:42:21 +0100

    make cleanup a function

--- cleanup.R
+++ cleanup.R
@@ -1,23 +1,25 @@
-
-# single threaded tmpfs cleanup ####
-if(file.exists(location.frida.storage)){
-	system(paste('rm', location.frida))
-	system(paste('mv',location.frida.storage,location.frida))
+cleanup <- function(){
+	# single threaded tmpfs cleanup ####
+	if(file.exists(location.frida.storage)){
+		system(paste('rm', location.frida))
+		system(paste('mv',location.frida.storage,location.frida))
+	}
+	if(file.exists(location.stella.storage)){
+		system(paste('rm', location.stella))
+		system(paste('mv',location.stella.storage,location.stella))
+	}
+	
+	
+	# cluster cleanup ####
+	cat('cluster cleanup...')
+	# stop cluster
+	if(exists('cl')){
+		tryCatch(stopCluster(cl),error=function(e){})
+		rm(cl)
+	}
+	# clean up working directories
+	unlink('workerDirs',recursive=T,force=T)
+	unlink(tmpfsDir,recursive=T,force=T)
+	cat('done\n')
 }
-if(file.exists(location.stella.storage)){
-	system(paste('rm', location.stella))
-	system(paste('mv',location.stella.storage,location.stella))
-}
-
-
-# cluster cleanup ####
-cat('cluster cleanup...')
-# stop cluster
-if(exists('cl')){
-	tryCatch(stopCluster(cl),error=function(e){})
-	rm(cl)
-}
-# clean up working directories
-unlink('workerDirs',recursive=T,force=T)
-unlink(tmpfsDir,recursive=T,force=T)
-cat('done\n')
+cleanup()
