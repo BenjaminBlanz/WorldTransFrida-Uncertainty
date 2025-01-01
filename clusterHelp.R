e040f858429d8e278f07f36b363dfedd0be30071
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-01-01 17:36:59 +0100

    make things available at the right time

--- clusterHelp.R
+++ clusterHelp.R
@@ -1,6 +1,10 @@
 
 library(parallel,quietly=T,warn.conflicts = F)
 
+
+baseWD <- getwd()
+workDirBasename <- 'workDir_'
+
 if(!exists('location.output')){
 	stop('run this only after config.R\n')
 }
@@ -14,6 +18,9 @@ if(exists('cl')&&is.numeric(result)&&result==2){
 	gobble <- clusterApply(cl,workers,function(i){
 		workerID <<- i
 	})
+	gobble <- clusterExport(cl,list('baseWD','workDirBasename'))
+	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'initialise.R')))
+	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'config.R')))
 	if(exists('sampleParms')){clusterExport(cl,list('sampleParms'))}
 	if(exists('calDat')){clusterExport(cl,list('calDat'))}
 	if(exists('resSigma')){clusterExport(cl,list('resSigma'))}
@@ -26,7 +33,6 @@ if(exists('cl')&&is.numeric(result)&&result==2){
 		library(tictoc)
 		library(SobolSequence,quietly = T)
 	}))
-	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'config.R')))
 	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'funRunFRIDA.R')))
 	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'funPlot.R')))
 	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'funParmSpace.R')))
@@ -38,23 +44,21 @@ if(exists('cl')&&is.numeric(result)&&result==2){
 		source('setupTMPFS.R')
 	}
 	
-	baseWD <- getwd()
-	workDirBasename <- 'workDir_'
 	# start cluster
 	if(clusterType=='fork'){
 		cl <- makeForkCluster(numWorkers)
 	} else if (clusterType=='psock'){
 		cl <- makePSOCKcluster(numWorkers,setup_strategy='sequential') # setup sequential to not DOS the server
 	}
+	gobble <- clusterExport(cl,list('baseWD','workDirBasename'))
 	gobble <- clusterEvalQ(cl,tools::psnice(value=15))
-	gobble <- clusterExport(cl,list('location.output','baseWD',
-												'chunkSizePerWorker','workDirBasename'))
 	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'initialise.R')))
+	gobble <- clusterEvalQ(cl,source(file.path(baseWD,'config.R')))
 	# make working directories
 	workers <- 1:length(cl)
 	gobble <- clusterApply(cl,workers,function(i){
 		workerID <<- i
-		Sys.sleep(workerID*0.5) # stagger the file copying to not DOS the server
+		Sys.sleep(workerID*0.1) # stagger the file copying to not DOS the server
 		system(paste('rm -r',file.path('workerDirs',paste0(workDirBasename,i))),ignore.stdout = T,ignore.stderr = T)
 		dir.create(file.path('workerDirs',paste0(workDirBasename,i)),showWarnings = F,recursive = T)
 		setwd(file.path('workerDirs',paste0(workDirBasename,i)))
@@ -62,7 +66,7 @@ if(exists('cl')&&is.numeric(result)&&result==2){
 	# clusterEvalQ(cl,getwd())
 	# copy over the model and simulator
 	gobble <- clusterApply(cl,workers,function(i){
-		Sys.sleep(workerID*0.5) # stagger the file copying to not DOS the server
+		Sys.sleep(workerID*0.1) # stagger the file copying to not DOS the server
 		file.copy(file.path(baseWD,location.frida),getwd(),recursive=T)
 		file.copy(file.path(baseWD,location.stella),getwd(),recursive=T)
 		file.copy(file.path(baseWD,'frida_info.csv'),getwd())
