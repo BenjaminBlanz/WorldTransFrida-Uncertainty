c38edae6b1386fd6b49b790153e87ed45c4acb2c
Lennart Ramme <lennart.ramme@mpimet.mpg.de>
2025-03-07 10:07:28 +0100

    Adding scripts for running on Levante

---
+++ TEMPLATE_runInitialiseData.R
@@ -0,0 +1,373 @@
+#
+#	provides a function to determine the likelihood of a given set of frida parameters
+#
+#
+# 2024 Benjamin Blanz
+# 
+
+library(caret,quietly=T,warn.conflicts = F) # to find linear combinations and remove them in the calib dat
+library(matrixcalc,quietly=T,warn.conflicts = F) # to test positive definitnes of cov matrix
+#library(imputeTS,quietly=T,warn.conflicts = F) # used for interpolating missing values # Only needed for interpolation after MLE, fails for R v4.4 on Levante
+
+if(!exists('runFridaDefaultParms')){
+	stop('run initialise.R first\n')
+}
+
+# test that FRIDA runs ####
+extraVarsToExport <- unique(read.csv(file.path(location.frida.info,name.frida_extra_variables_to_export_list))$FRIDA.FQN)
+extraVarsToExport <- extraVarsToExport[nchar(extraVarsToExport)>4]
+writeFRIDAExportSpec(extraVarsToExport,location.frida)
+test <- runFridaDefaultParms(silent=F)
+continue <- readline('Verify there are no errors in the above.\n  Enter for all clear, Ctrl+C for abort\n')
+
+# read calib data ####
+cat('reading calibration data ...')
+calDat.orig <- read.csv(file.path(location.frida,'Data','Calibration Data.csv'), header=FALSE,
+									 colClasses = "character") 
+# character class for input as in this step we do not want to change the data in any
+# way
+
+# store names of vars for export in frida format (including dots etc)
+# but remove trailing spaces as stella does not like those apparently
+# also remove first entry, that is time
+varsForExport.fridaNames.orig <- gsub(' $','',calDat.orig$V1)[-1]
+varsForExport.cleanNames.orig <- cleanNames(varsForExport.fridaNames.orig)
+
+# kick out duplicates
+if(any(duplicated(calDat.orig))){
+	cat('\n  removing duplicate rows in calibration data\n ',
+			paste0(calDat.orig$V1[duplicated(calDat.orig)],collapse = '\n  '))
+	calDat.orig <- calDat.orig[!duplicated(calDat.orig),]
+}
+if(any(duplicated(calDat.orig$V1))){
+	stop('\n  There are duplicated variable names with non identical data!\n ',
+			 paste0(calDat.orig$V1[duplicated(calDat.orig$V1)],collapse = '\n '))
+}
+calDat.orig <- t(calDat.orig)
+# first row of the transpose table is the column headers
+colnames(calDat.orig) <- calDat.orig[1,]
+# drop the first column that conatins column headers
+calDat.orig <- calDat.orig[-1,]
+# drop all rows after the empty row signifying the end of the data
+calDat.orig <- calDat.orig[-seq(which(rowSums(calDat.orig!='')==0)[1],nrow(calDat.orig)),]
+# write out the transposed and cleaned calDat. Not sure it needs to be this complicaetd
+# but all other methods changed the data in situ.
+sink(file.path(location.output,'Calibratio_Data_Cleaned_and_Transposed.csv'))
+cat(paste(paste0('"',colnames(calDat.orig),'"'),collapse=','))
+cat('\n')
+for(r in 1:nrow(calDat.orig)){
+	cat(paste(calDat.orig[r,],collapse=','))
+	cat('\n')
+}
+sink()
+# read in the cleaned and transformed file, this time read numbers as numbers etc.
+calDat.orig <- read.csv(file.path(location.output,'Calibratio_Data_Cleaned_and_Transposed.csv'))
+colnames(calDat.orig) <- cleanNames(colnames(calDat.orig))
+rownames(calDat.orig) <- calDat.orig$year
+calDat <- calDat.orig <- calDat.orig[,-1]
+cat('done\n')
+
+# impute missing vars ####
+if(imputeMissingVars||extrapolateMissingVarMethod!='n'){
+	ncols <- ncol(calDat)
+	for(i in 1:ncol(calDat)){
+		## setup
+		x <- calDat.orig[[i]]
+		validRange <- funValidRange(x)
+		## impute 
+		if(imputeMissingVars){
+			x[validRange[1]:validRange[2]] <- na_interpolation(x[validRange[1]:validRange[2]])
+		}
+		## extrapolate
+		if(extrapolateMissingVarMethod=='f'){
+			if(validRange[1]>1){
+				x[1:(validRange[1]-1)] <- x[validRange[1]]
+			}
+			if(validRange[2]<length(x)){
+				x[(validRange[2]+1):length(x)] <- x[validRange[2]]
+			}
+		}
+		## write back
+		calDat[[i]] <- x
+	}
+} 
+calDat.impExtrValue <- calDat.orig
+calDat.impExtrValue <- is.na(calDat.orig)&!is.na(calDat)
+calDat.afterImpute <- calDat
+
+# determine Vars to Exclude ####
+cat('Applying rules to exclude variables from likelihood analysis...\n')
+# exclude all that have fewer than three data points as we
+# can not determine variance in those cases.
+numNA <- c()
+for(i in 1:ncol(calDat.afterImpute)){
+	numNA[i] <- sum(is.na(calDat.afterImpute[,i]))
+}
+exclude.idc <- which((nrow(calDat.afterImpute)-numNA) < minObsForLike)
+
+cat(sprintf('  Excluding for less than minObsForLike (%i) observations:\n    ',minObsForLike))
+cat(paste(colnames(calDat.afterImpute)[exclude.idc],collapse='\n    '))
+cat('\n')
+calDat <- calDat.afterImpute[,-exclude.idc]
+
+# remove manual exclusion list ####
+if(file.exists(file.path(location.frida.info,name.frida_variable_exclusion_list))&&file.size(file.path(location.frida.info,name.frida_variable_exclusion_list))>0){
+	manExclusionList <- read.csv(file.path(location.frida.info,name.frida_variable_exclusion_list))
+	exclude.idc <- c(exclude.idc,which(varsForExport.fridaNames.orig %in% manExclusionList$Variable))
+	cat(paste('  Excluding because of manual exclusion list',manExclusionList$Variable,'\n'))
+}
+
+# exclude vars ####
+# do not exclude from zero var in obs, only resid below
+if(F){
+	doneExcluding <- F
+	while(!doneExcluding){
+		doneExcluding <- T
+		# calc resid
+		calDat <- calDat.afterImpute[,-exclude.idc]
+		varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+		writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+		defDat <- runFridaDefaultParms()
+		if(sum(colnames(defDat)!=colnames(calDat))!=0){
+			stop('Mismatch in the columns of calibration data and model result data')
+		}
+		resDat <- defDat[1:nrow(calDat),]-calDat
+		
+		# check for zero var vars in resid ####
+		for(i in 1:ncol(resDat)){
+			if(var(resDat[,i],na.rm=T)==0){
+				cat(sprintf('  Excluding %s for zero variance in resid\n',
+						colnames(resDat)[i]))
+				doneExcluding <- F
+				exclude.idc <- c(exclude.idc,
+												 which(varsForExport.cleanNames.orig==colnames(resDat)[i]))
+			}
+		}
+	}
+}
+
+# kick out zero variance in resid ####
+calDat <- calDat.afterImpute[,-exclude.idc]
+varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+defDat <- runFridaDefaultParms()
+if(sum(colnames(defDat)!=colnames(calDat))!=0){
+	stop('Mismatch in the columns of calibration data and model result data')
+}
+resDat <- defDat[1:nrow(calDat),]-calDat
+for(i in 1:ncol(resDat)){
+	res.sd <- sd(resDat[[i]],na.rm=T)
+	if(res.sd==0){
+		exclude.idc <- c(exclude.idc,which(varsForExport.cleanNames.orig==colnames(resDat)[i]))
+		cat(sprintf('Excluding %s for zero variance in resid\n',colnames(resDat)[i]))
+	}
+}
+	
+if(!treatVarsAsIndep){	
+	# check for perfect corr in resid ####
+	# calc resid
+	calDat <- calDat.afterImpute[,-exclude.idc]
+	varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+	writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+	defDat <- runFridaDefaultParms()
+	if(sum(colnames(defDat)!=colnames(calDat))!=0){
+		stop('Mismatch in the columns of calibration data and model result data')
+	}
+	resDat <- defDat[1:nrow(calDat),]-calDat
+	resDat.cor <- cor(resDat,use='complete.obs')
+	perfCors <- which((resDat.cor-diag(1,ncol(resDat)))==1,arr.ind=T)
+	if(nrow(perfCors)>0){
+		for(i in 1:nrow(perfCors)){
+			if(!(colnames(resDat)[perfCors[i,2]] %in% varsForExport.cleanNames.orig[exclude.idc])){
+				cat(sprintf('  Excluding %s for perfect cor. with %s\n',
+										colnames(resDat)[perfCors[i,1]],colnames(resDat)[perfCors[i,2]]))
+				exclude.idc <- c(exclude.idc,
+												 which(varsForExport.cleanNames.orig==colnames(resDat)[perfCors[i,1]]))
+			}
+		}
+	}
+}
+
+# find linear combinations ####
+if(removeLinearCombinations&&!treatVarsAsIndep){
+	##  in calDat ####
+	# new resid
+	calDat <- calDat.afterImpute[,-exclude.idc]
+	varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+	writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+	defDat <- runFridaDefaultParms()
+	if(sum(colnames(defDat)!=colnames(calDat))!=0){
+		stop('Mismatch in the columns of calibration data and model result data')
+	}
+	resDat <- defDat[1:nrow(calDat),]-calDat
+	linearCombos <- caret::findLinearCombos(resDat[complete.cases(resDat),])
+	if(length(linearCombos$remove)>0){
+		new.exclude.idc <- idxOfVarName(colnames(calDat)[linearCombos$remove],
+																		varsForExport.cleanNames.orig)
+		cat('  Excluding because they are linear combinations of other vars:\n    ')
+		cat(paste(varsForExport.cleanNames.orig[new.exclude.idc],collapse='\n    '))
+		cat('\n')
+		exclude.idc <- unique(c(exclude.idc,new.exclude.idc))
+	}
+	
+	
+	## in calDat.cv ####
+	# new resid
+	calDat <- calDat.afterImpute[,-exclude.idc]
+	varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+	writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+	defDat <- runFridaDefaultParms()
+	if(sum(colnames(defDat)!=colnames(calDat))!=0){
+		stop('Mismatch in the columns of calibration data and model result data')
+	}
+	resDat <- defDat[1:nrow(calDat),]-calDat
+	
+	tryCatch({resDat.cv <- cov(resDat,use='complete.obs')},
+					 error=function(e){resDat.cv <- NA})
+	if(sum(is.na(resDat.cv))==0){
+		if(is.singular.matrix(resDat.cv)){
+			linearCombos <- caret::findLinearCombos(calDat[complete.cases(calDat),])
+			if(length(linearCombos$remove)>0){
+				new.exclude.idc <- idxOfVarName(colnames(calDat)[linearCombos$remove],
+																				varsForExport.cleanNames.orig)
+				cat('  Excluding because they are linear combinations of other vars:\n    ')
+				cat(paste(varsForExport.cleanNames.orig[new.exclude.idc],collapse='\n    '))
+				cat('\n')
+				exclude.idc <- unique(c(exclude.idc,new.exclude.idc))
+			}
+		}
+	}
+	calDat <- calDat.afterImpute[,-exclude.idc]
+	cat('...done\n')
+}
+
+cat(sprintf('After exclusion we are left with %i out of %i variables\n',ncol(resDat),length(varsForExport.cleanNames.orig)))
+
+
+# remove excluded idc ###
+calDat.withAllVars <- calDat.orig
+calDat.orig <- calDat.orig[,-exclude.idc]
+calDat <- calDat.afterImpute[,-exclude.idc]
+calDat.impExtrValue <- calDat.impExtrValue[,-exclude.idc]
+rm(calDat.afterImpute)
+
+# clean def run ####
+varsForExport.fridaNames <- varsForExport.fridaNames.orig[-exclude.idc]
+writeFRIDAExportSpec(varsForExport.fridaNames,location.frida)
+defDat <- runFridaDefaultParms()
+resDat <- defDat[1:nrow(calDat),]-calDat
+
+# data Plots ####
+if(plotWhileRunning){
+	funPlotDat(calDat,calDat.impExtrValue,defDat)
+	dev.print(pdf,
+						file.path(location.output,'calDatPlot.pdf'))
+	# funPlotDat(resDat,calDat.impExtrValue)
+	# dev.print(pdf,
+	# 					file.path(location.output,'resDatPlot.pdf'))
+}
+
+# covar ####
+if(sum(colnames(defDat)!=colnames(calDat))!=0){
+	stop('Mismatch in the columns of calibration data and model result data')
+}
+
+
+cat('Determining the distribution of the residuals in the default case...\n')
+if(treatVarsAsIndep){
+	variances <- NA
+	for(i in 1:ncol(resDat)){
+		variances[i] <- var(resDat[[i]],na.rm=T)
+	}
+	resDat.cv <- diag(variances)
+} else {
+	resDat.cv <- NA
+	# this is not the correct way to do it. May not yield a positive definit matrix.
+	# but if it is valid uses more of the observations, so try it.
+	# See the following for why this is bad.
+	# https://www.r-bloggers.com/2015/06/pairwise-complete-correlation-considered-dangerous/
+	cat('  trying pariwise complete obs...')
+	try({resDat.cv <- cov(resDat,use='pairwise.complete.obs')})
+	if(sum(is.na(resDat.cv))>0||!is.positive.definite(resDat.cv)){
+		cat('fail\n')
+		resDat.cv <- NA
+	} else {
+		cat('success\n')
+	}
+	if(sum(is.na(resDat.cv))>0){
+		cat(sprintf('  trying for complete obs, using %i obs...',sum(complete.cases(resDat))))
+		tryCatch({resDat.cv <- cov(resDat,use='complete.obs')},
+						 error=function(e){resDat.cv <- NA})
+		if(sum(is.na(resDat.cv))>0){
+			cat('fail\n')
+			resDat.cv <- NA
+		} else {
+			cat('success\n')
+		}
+	}
+	# if there are no comple obs, give this a try but make it be quiet.
+	if(sum(is.na(resDat.cv))>0){
+		cat('fail\n  trying FitGMMM...')
+		sinkFile <- file('FitGMMM_babble','w')
+		sink(sinkFile,type=c('message'))
+		sink('FitGMM_babble2')
+		try({
+			resDat.mvn <- FitGMM(as.matrix(resDat),
+													 init_means = list(a=colMeans(resDat,na.rm=T)),
+													 init_covs = list(a=diag(1,nrow = ncol(resDat), ncol=ncol(resDat))),
+													 #fix_means = T,
+													 maxit = 1e5,
+													 eps = 1e-6)
+			resDat.mu <- resDat.mvn@Mean
+			resDat.sigma <- resDat.mvn@Covariance
+		})
+		sink(NULL,type='message')
+		sink()
+		close(sinkFile)
+		if(sum(is.na(resDat.cv))>0){
+			cat('fail\n')
+			resDat.cv <- NA
+		} else {
+			cat('success\n')
+		}
+	}
+	if(sum(is.na(resDat.cv))>0){
+		stop('The covariance matrix could not be calculated\n')
+	}
+	if(is.negative.definite(resDat.cv)){
+		stop('The estimated covariance matrix is negative definite.\nLikelihood can not be calculated\n')
+	}
+	if(is.singular.matrix(resDat.cv)){
+		cat('Estimated covarianve matrix is singular.\nMight be a problem.\n')
+	} else {
+		cat('Estimated covariance matrix is fine\n')
+	}
+}
+
+# likelihood ####
+if(treatVarsAsIndep){
+	defLogLike <- funLogLikelihood(resDat,resDat.cv)
+} else {
+	defLogLike <- funLogLikelihood(resDat[complete.cases(resDat),],resDat.cv)
+}
+cat(paste0('Log Likelihood in the default case: ',defLogLike,'\n'))
+defLike <- exp(mpfr(defLogLike,64))
+cat(paste0('Likelihood in the default case: ',formatMpfr(defLike),'\n'))
+if(is.infinite(defLike)||is.na(defLike)){
+	stop('Bad default log likelihood\n')
+}
+
+# save run prep ####
+writeFRIDAExportSpec(varsForExport.fridaNames.orig[-exclude.idc],location.frida)
+if(exists('location.frida.storage')&&file.exists(location.frida.storage)){
+	writeFRIDAExportSpec(varsForExport.fridaNames.orig[-exclude.idc],location.frida.storage)
+}
+saveRDS(resDat.cv,file.path(location.output,'sigma.RDS'))
+saveRDS(diag(diag(resDat.cv)),file.path(location.output,'sigma-indepParms.RDS'))
+saveRDS(list(
+	calDat=calDat,
+	calDat.impExtrValue=calDat.impExtrValue,
+	calDat.orig=calDat.orig,
+	calDat.withAllVars=calDat.withAllVars),
+	file.path(location.output,'calDat.RDS'))
+
---
+++ submit_UncertaintyAnalysisLevante.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+##############################################################################
+########                  Change input here                           ########
+##############################################################################
+# If you want to change other things, these need to be changed in the
+# respective .R files directly! Or, ideally, add the option to change them also
+# here in the script. Any change to e.g. config.R or runMLEandParmSpace.R will
+# always be used in the future, as these are the templates for this script!
+
+expID=UA_nW250_nS10000_maxC1024_no2
+
+numWorkers=250
+numSamples=1.0e4
+maxConnections=1024
+
+# How long will it take approximately? Job will be killed after this time!
+# But max 8:00 hours and the shorter the run is, the earlier the job gets run
+hours=8
+minutes=00
+
+# Enter Email here in case you want to receive a mail, when the job failed
+email=Testmailforscript@UncertaintyAnalysis.abc
+email=lennart.ramme@mpimet.mpg.de
+
+# Copy ranges from pre-existing work?
+copyParmRangesAndScales="false"
+copyID=UA_XYZ   # This should be the name of workOutput from which the files will be copied!
+
+
+
+#### OPTIONAL: USING DIFFERENT INPUT FILES?
+# FRIDA folder
+FRIDA='FRIDAforSLRpaper'
+
+# These need to be located in the FRIDA-configs/ folder!
+policyFile='policy_EMB.csv'
+
+# These need to be located in the FRIDA-info/ folder!
+infoFile='frida_info.csv'
+externalRangesFile='frida_external_ranges.csv'
+excludeParmFile='frida_parameter_exclusion_list.csv'
+excludeVarFile='frida_variable_exclusion_list.csv'
+extraExportFile='frida_extra_variables_to_export_list.csv'
+
+
+##############################################################################
+
+
+#############################################################################
+############ Preparing the R-scripts and the runscript ######################
+#############################################################################
+
+# Modify the config file according to the settings above
+config=${expID}_config.R
+cp TEMPLATE_config.R $config
+
+sed -i "s/numWorkers <- 120/numWorkers <- ${numWorkers}/" $config
+sed -i "s/numSample <- 1e4/numSample <- ${numSamples}/" $config
+sed -i "s/file.path('workOutput',name.output)/file.path('workOutput','${expID}')/" $config
+sed -i "s/config.R/${config}/g" $config
+sed -i "s/FRIDAforUncertaintyAnalysis/${FRIDA}/" $config
+sed -i "s/frida_info.csv/${infoFile}/" $config
+sed -i "s/frida_external_ranges.csv/${externalRangesFile}/" $config
+sed -i "s/frida_parameter_exclusion_list.csv/${excludeParmFile}/" $config
+sed -i "s/frida_variable_exclusion_list.csv/${excludeVarFile}/" $config
+sed -i "s/frida_extra_variables_to_export_list.csv/${extraExportFile}/" $config
+
+
+# Need to also modify the runMLE and runInitData, to avoid the breaks in R 
+# that wait for someone to hit enter and to use the updated config
+
+# modify runInitialise
+runInit=${expID}_runInitialiseData.R
+cp TEMPLATE_runInitialiseData.R $runInit
+
+sed -i "/^continue <- readline/d" $runInit
+
+# modify clusterHelp to use correct config
+clusterHelp=${expID}_clusterHelp.R
+cp TEMPLATE_clusterHelp.R $clusterHelp
+
+sed -i "s/config.R/${config}/" $clusterHelp
+
+# modify runMLE
+runMLE=${expID}_runMLE.R
+cp TEMPLATE_runMLEandParmSpace.R $runMLE
+
+sed -i "s/config.R/${config}/g" $runMLE
+sed -i "s/runInitialiseData.R/${runInit}/g" $runMLE
+sed -i "s/clusterHelp.R/${clusterHelp}/g" $runMLE
+sed -i "/^continue <- readline/d" $runMLE
+
+# Create the runscript from the template
+template='UncertaintyAnalysis.run'
+runscript="${expID}.run"
+#runscript="${expID}_numW=${numWorkers}_numS=${numSamples}_maxC=${maxConnections}.run"
+
+cp $template $runscript
+
+# Modify the template
+sed -i "s/time=01:00:00/time=0${hours}:${minutes}:00/" $runscript
+sed -i "s/LOG.UncertaintyAnalysis/LOG.${expID}/g" $runscript
+sed -i "s/jdoe@mail.com/${email}/" $runscript
+sed -i "s/job-name=runUncertaintyAnalysis/job-name=${expID}/" $runscript
+sed -i "s/--max-connections=1024/--max-connections=${maxConnections}/" $runscript
+sed -i "s/runMLEandParmSpace.R/${runMLE}/" $runscript
+
+##############################################################################
+
+#############################################################################
+############ Copying parameter ranges and scales ############################
+#############################################################################
+
+expDir="workOutput/${expID}"
+if [ -d "$expDir" ]; then
+	echo $expDir already exists       
+else
+	mkdir $expDir
+fi
+
+
+if [ "$copyParmRangesAndScales" = "true" ]; then
+	for filename in 'parscale.RDS' 'sampleParmsParscale.csv' 'sampleParmsParscaleRanged.csv' 'sampleParmsParscaleanged.RDS' 'samplePointsBase.RDS' 'samplePoints.RDS' 'sigma-indepParms.RDS' 'sigma.RDS'; do
+		file=${filename}
+		if [ -e $file ]; then
+			cp workOutput/${copyID}/${file}  ${expDir}/
+		else
+			echo "File ${file} not found, exiting"
+			exit 1
+		fi
+	done
+fi
+
+##############################################################################
+
+
+#############################################################################
+#######                    Submit the Job                            ########
+#############################################################################
+sbatch $runscript
