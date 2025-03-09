79c2ea65a5905a514bcdb87e61f628b861d0d844
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-03-09 17:10:27 +0100

    deal with duplicates stemming from capitalisation

--- runPlotAllRuns.R
+++ runPlotAllRuns.R
@@ -88,6 +88,7 @@ for(plotWeightType in plotWeightTypes){
 	for(varName.i in 1:length(allVarNames)){
 		varName <- allVarNames[varName.i]
 		varName.orig <- allVarNames.orig[which(allVarNames==varName)]
+		if(length(varName.orig)>1){varName.orig<-varName.orig[1]}
 		cat(sprintf('(%i of %i) Plotting %s...\n  ',
 								varName.i,length(allVarNames),varName.orig))
 		cat('read...')
