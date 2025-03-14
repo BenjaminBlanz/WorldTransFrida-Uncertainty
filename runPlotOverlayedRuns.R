8bf3d311c44761c0e1783285ca2da173d6f14185
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-03-14 15:54:52 +0100

    automatically fit y axes for all lines in the plot
    add commented code to produce jeffs figures

--- runPlotOverlayedRuns.R
+++ runPlotOverlayedRuns.R
@@ -2,21 +2,80 @@
 # uses output of the runPlotAllRuns script, so that has to be run first for each 
 # overlayed ensemble
 
-# just for plot specification
-plotWeightType <- 'equaly'
+# specify output folders
 dataForOverlayedFiguresFolders <- c('workOutput/JeffEMB_figures/CI-plots/equalyWeighted/plotData/',
 																		'workOutput/JeffGDP_figures/CI-plots/equalyWeighted/plotData/')
-overlayNames <- c('EMB','GDP driven food demand')
 overlayColors <- c('black','blue')
 
-location.plots <- file.path('workOutput','jeffsOverlayedFigures',uncertaintyType)
+# For specialised overlayed figures:
+# prepare folder structure such that folders for each of the names exist, with only a single file contained
+# then uncomment sections for the individual figues into runPlotOverlayRuns above everything
+# this will eventually become a more streamlined process, outomatically picking the diesired files...
+
+# Jeff's Fig 1
+# extraTitle <- 'fig 1'
+# overlayNames <- c('Desired Animal Products Demand from Accessibility','Desired Animal Products Demand from Descriptive Norm',
+# 									'Desired Animal Products Demand from Personal Norm')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue','red')
+
+# Jeff's Fig 2
+# extraTitle <- 'fig 2'
+# overlayNames <- c('Desired Food Demand from Accessibility',
+# 									'Desired Food Demand from Descriptive Norm',
+# 									'Desired Food Demand from Personal Norm')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue','red')
+
+# Jeff's Fig 3
+# extraTitle <- 'fig 3 climate extremes'
+# overlayNames <- c('Perceived Exposure',
+# 									'Reference Normal Exposure')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue')
+
+# Jeff's Fig 4
+# extraTitle <- 'fig 4 slr flooding'
+# overlayNames <- c('Perceived Exposure',
+# 									'Reference Normal Exposure')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue')
+
+# Jeff's Fig 5
+# extraTitle <- 'fig 5 sta'
+# overlayNames <- c('Perceived Exposure',
+# 									'Reference Normal Exposure')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue')
+
+# Jeff's Fig 6
+# Could you please help me overlay 
+# food demand.animal product demand per person per day (GDP driven) 
+# with 
+# animal product demand.average daily demand per capita (EMB). 
+# This would include the calibration data.
+# extraTitle <- 'fig 6 food demand'
+# overlayNames <- c('animal product demand per person per day (GDP driven)',
+# 									'average daily demand per capita (EMB)')
+# dataForOverlayedFiguresFolders <- paste0('workOutput/jeffsFigures/',extraTitle,'/',overlayNames)
+# overlayColors <- c('black','blue')
+
+
+# just for plot specification
+plotWeightType <- 'equaly'
+
+location.plots <- file.path('workOutput','jeffsOverlayedFigures','fit uncertainty')
 dir.create(location.plots,F,T)
 for(file in list.files(dataForOverlayedFiguresFolders[1],pattern = '*RDS')){
 	if(sum(file.exists(file.path(dataForOverlayedFiguresFolders,file)))==length(dataForOverlayedFiguresFolders)){
 		varName <- tools::file_path_sans_ext(file)
 		png(file.path(location.plots,paste0(varName,'.png')),
 				width = plotWidth,height = plotHeight,units = plotUnit,res = plotRes)
-		plotData <- readRDS(file.path(dataForOverlayedFiguresFolders[1],file))
+		plotData.lst <- list()
+		for(o.i in 1:length(dataForOverlayedFiguresFolders)){
+			plotData.lst[[o.i]] <- readRDS(file.path(dataForOverlayedFiguresFolders[o.i],file))
+		}
+		plotData <- plotData.lst[[1]]
 		yearsToPlot <- plotData$years
 		uncertaintyType <- plotData$uncertaintyType
 		ciBoundQs <- plotData$ciBoundQs
@@ -29,17 +88,8 @@ for(file in list.files(dataForOverlayedFiguresFolders[1],pattern = '*RDS')){
 		ciBoundQs.lty <- c(rev(CIsToPlot.lty),CIsToPlot.lty[-1])
 		ciBoundQs.lwd <- c(rev(CIsToPlot.lwd),CIsToPlot.lwd[-1])
 		ciBoundQs.lcol <- c(rev(CIsToPlot.lcol),CIsToPlot.lcol[-1])
-		plotData <- readRDS(file.path(dataForOverlayedFiguresFolders[o.i],file))
-		yearsToPlot <- plotData$years
-		uncertaintyType <- plotData$uncertaintyType
-		ciBoundQs <- plotData$ciBoundQs
-		ciBounds <- plotData$ciBounds
-		means <- plotData$means
-		defRun <- plotData$defaultRun
-		varName.orig <- plotData$varName.orig
-		calDat <- plotData$calDat
 		medianQIdx <- which(ciBoundQs==0.5)
-		layout(matrix(c(3,2,1),nrow=3),heights = c(0.9,0.05,0.05))
+		layout(matrix(c(3,2,1),nrow=3),heights = c(0.9,0.05,0.04))
 		par(mar=c(0,0,0,0))
 		plot(0,0,type='n',axes=F)
 		legend.text=c(
@@ -70,17 +120,23 @@ for(file in list.files(dataForOverlayedFiguresFolders[1],pattern = '*RDS')){
 		par(mar=c(0,0,0,0))
 		plot(0,0,type='n',axes=F)
 		legend('bottom',overlayNames,pch=15,col=overlayColors,
-					 horiz=T,xpd=T)
-		par(mar=c(3.1,4.1,4.1,2.1))
+					 horiz=F,xpd=T,ncol=2)
+		par(mar=c(4.1,4.1,4.1,2.1))
+		ymax <- NA
+		ymin <- NA
+		for(o.i in 1:length(dataForOverlayedFiguresFolders)){
+			ymax <- max(ymax,plotData.lst[[o.i]]$ciBounds,na.rm=T)
+			ymin <- min(ymin,plotData.lst[[o.i]]$ciBounds,na.rm=T)
+		}
 		plot(yearsToPlot,ciBounds[yearsToPlot,medianQIdx],
-				 ylim=range(c(ciBounds[yearsToPlot,c(2,length(ciBoundQs)-1)],calDat[yearsToPlot]),na.rm=T),
+				 ylim=c(ymin,ymax),
 				 xlim=range(as.numeric(yearsToPlot)),
 				 xaxs='i',
 				 type='n',
 				 xlab='',
-				 ylab=varName.orig,
+				 ylab=ifelse(exists('extraTitle'),extraTitle,varName.orig),
 				 xaxt='n',
-				 main=varName.orig)
+				 main=ifelse(exists('extraTitle'),extraTitle,varName.orig))
 		mtext(paste('Samples',plotWeightType,'weighted. Ranges show ',uncertaintyType,'.'),3,0.5,cex=par('cex'))
 		xax <- axis(1,at=seq(as.numeric(yearsToPlot[1]),as.numeric(yearsToPlot[length(yearsToPlot)]),10))
 		mtext('year',1,3)
@@ -88,14 +144,13 @@ for(file in list.files(dataForOverlayedFiguresFolders[1],pattern = '*RDS')){
 		abline(h=axTicks(2),lty='dotted',col='gray')
 		for(o.i in 1:length(dataForOverlayedFiguresFolders)){
 			ciBoundQs.lcol <- rep(overlayColors[o.i],length(ciBoundQs.lcol))
-			plotData <- readRDS(file.path(dataForOverlayedFiguresFolders[o.i],file))
+			plotData <- plotData.lst[[o.i]]
 			yearsToPlot <- plotData$years
 			uncertaintyType <- plotData$uncertaintyType
 			ciBoundQs <- plotData$ciBoundQs
 			ciBounds <- plotData$ciBounds
 			means <- plotData$means
 			defRun <- plotData$defaultRun
-			varName.orig <- plotData$varName.orig
 			calDat <- plotData$calDat
 			for(ci.i in length(CIsToPlot.col):1){
 				CIsToPlot.col[ci.i] <- adjustcolor(overlayColors[o.i],0.4/ci.i)
@@ -125,7 +180,6 @@ for(file in list.files(dataForOverlayedFiguresFolders[1],pattern = '*RDS')){
 			ciBounds <- plotData$ciBounds
 			means <- plotData$means
 			defRun <- plotData$defaultRun
-			varName.orig <- plotData$varName.orig
 			calDat <- plotData$calDat
 			for(q.i in 1:length(ciBoundQs)){
 				lines(yearsToPlot,ciBounds[yearsToPlot,q.i],
