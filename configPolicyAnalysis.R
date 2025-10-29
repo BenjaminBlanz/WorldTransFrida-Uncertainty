5d361b77275b4cf1938505c3b05b94a6326f16f4
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-10-30 00:45:50 +0100

    plotTypes

--- configPolicyAnalysis.R
+++ configPolicyAnalysis.R
@@ -116,7 +116,7 @@ alsoPlotDefaultRun.vals <- c(TRUE,FALSE)
 def.lty <- 'solid'
 def.lwd <- 2
 def.col <- 'green'
-plotType <- 2
+plotTypes <- c(0,2,3)
 plot.numColLevels <- 26
 plot.palletteName <- "grDevices::RdPu"
 plot.palletteNameSOW <- "grDevices::Greens"
