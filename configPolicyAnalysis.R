540d5b16bd259486d9c4533d24e0e26829051267
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-11-10 14:10:43 +0100

    reduce threads back to 15, due to experienced out of memory situations.

--- configPolicyAnalysis.R
+++ configPolicyAnalysis.R
@@ -162,7 +162,7 @@ selectedRunEnsemble.lty <- 2
 selectedRunEnsemble.lwd <- 1
 selectedRunEnsemble.col <- 'black'
 
-numPlotThreads <- 20
+numPlotThreads <- 15
 location.plots <- 'figures'
 # plot 
 plotTypes <- c(2,3)
