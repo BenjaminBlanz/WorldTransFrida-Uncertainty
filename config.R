1e479ed698ca5faa02a4185633102d3e6ce7a6b9
Benjamin Blanz <benjamin.blanz@uni-hamburg.de>
2025-12-10 14:57:48 +0100

    change default subSample.TargetVars to STA

--- config.R
+++ config.R
@@ -125,7 +125,7 @@ if(!exists('skipParMLE')){
 subSample.NumSamplePerVar <- 11
 subSample.Ps <- seq(0.5/subSample.NumSamplePerVar,1-0.5/subSample.NumSamplePerVar,
 										length.out=subSample.NumSamplePerVar)
-subSample.TargetVars <- c('demographics_real_gdp_per_person')
+subSample.TargetVars <- c('energy_balance_model_surface_temperature_anomaly')
 
 
 # FRIDA config ####
